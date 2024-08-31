9f5851fe9a2093e5d04681598dbf28b273059cb3
Fernanda-Vitoria-etec <ferferreira929@gmail.com>
2024-08-31 20:42:19 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(" ||  || ");
+        Console.WriteLine(@" \\()// ");
+        Console.WriteLine(@"//(__)\\");
+        Console.WriteLine("||    ||");
+    }
+}
